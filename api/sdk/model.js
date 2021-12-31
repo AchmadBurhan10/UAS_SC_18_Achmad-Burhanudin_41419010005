b027c26434124bc2ab68d98056605e7f840643f0
AchmadBurhan10 <96718827+AchmadBurhan10@users.noreply.github.com>
2021-12-31 13:37:38 +0700

    Update model.js

--- api/sdk/model.js
+++ api/sdk/model.js
@@ -1,43 +1,106 @@
-const tf = require('@tensorflow/tfjs-node');
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@AchmadBurhan10 
+AchmadBurhan10
+/
+UAS_SC_18_Achmad-Burhanudin_41419010005
+Public
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+Settings
+UAS_SC_18_Achmad-Burhanudin_41419010005/api/index.js /
+@AchmadBurhan10
+AchmadBurhan10 Update index.js
+Latest commit 3f0ea51 2 hours ago
+ History
+ 1 contributor
+78 lines (69 sloc)  1.8 KB
+   
+var express = require('express');
+var r = express.Router();
 
-function normalized(data){ // i & r
-    i = (data[0] - 12.585) / 6.813882
-    r = (data[1] - 51.4795) / 29.151289
-    return [i, r]
-}
+// load pre-trained model
+const model = require('./sdk/model.js');
 
-function denormalized(data){
-    v = (data[0] * 552.6264) + 650.4795
-    p = (data[1] * 12153.8) + 10620.5615
-    return [v, p]
-}
+// Bot Setting
+const TelegramBot = require('node-telegram-bot-api');
+const token = '5073546476:AAG4rie_ytVRpRfHLYNLt0cDz_YcjDidZAE'
+const bot = new TelegramBot(token, {polling: true});
 
 
-async function predict(data){
-    let in_dim = 2;
-    
-    data = normalized(data);
-    shape = [1, in_dim];
+// bots
+bot.onText(/\/start/, (msg) => { 
+    console.log(msg)
+    bot.sendMessage(
+        msg.chat.id,
+        `halo selamat datang ! ${msg.chat.first_name}, welcome...\n
+        click /prediksi`
+    );   
+});
+// input requires x1 , x2 dan x3
+state = 0;
+bot.onText(/\/prediksi/, (msg) => { 
+    bot.sendMessage(
+        msg.chat.id,
+        `Masukan nilai x1|x2|x3 contohnya 8|8|8`
+    );
+    state = 1;
+});
 
-    tf_data = tf.tensor2d(data, shape);
-
-    try{
-        // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/AchmadBurhan10/UAS_SC_18_Achmad-Burhanudin_41419010005/main/public/ex_model/model.json';
-        const model = await tf.loadGraphModel(path);
-        
-        predict = model.predict(
-                tf_data
-        );
-        result = predict.dataSync();
-        return denormalized( result );
-        
-    }catch(e){
-      console.log(e);
+bot.on('message', (msg) => (
+    if(state == 1){
+        s = msg.text.split("|");
+        x1 = s[0]
+        x2 = s[1]
+        x3 = s[2]
+        model.prediksi(
+            [ 
+                parsefloat(s[0]), // string float
+                parsefloat(s[1]),
+                parsefloat(s[2])
+           
+            ]
+        ).them((jres)=>{
+            bot.sendmessage(
+                msg.chat.id,
+                'Nilai y1 yang diprediksi adalah ${jres[0])'
+            );  
+            bot.sendmessage(
+                msg.chat.id,
+                'Nilai y2 yang diprediksi adalah ${jres[1])'
+            );
+            bot.sendmessage(
+                msg.chat.id,
+                'Nilai y3 yang diprediksi adalah ${jres[2])'
+             );
+        })
+    }else{
+        state = 0
     }
-}
+})
 
-module.exports = {
-    predict: predict 
-}
-  
+// routers
+r.get('prediksi/:x1/:x2/:x3', function(req, res, next) {
+    model.prediksi(
+        [
+            parsefloat(req.params.x1),// string float
+            parsefloat(req.params.x2),
+            parsefloat(req.params.x3)
+        ]    
+    }.then((jres)=>{
+        res.json(jres);
+    })
+});
+
+module.exports = r:
